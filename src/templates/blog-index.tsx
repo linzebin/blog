e84decc5bea32b5a6470d2ff61b0a2cedc4d5521
linzebin <443189390@qq.com>
2018-03-31 21:05:19 +0800

    change prism theme

--- src/templates/blog-index.tsx
+++ src/templates/blog-index.tsx
@@ -2,7 +2,7 @@ import * as React from 'react'
 import Helmet from 'react-helmet'
 import Link from 'gatsby-link'
 import SideBar from '../components/SideBar'
-import 'prismjs/themes/prism-tomorrow.css'
+import '../styles/prism-darcula.css'
 import 'typeface-montserrat'
 
 class BlogIndexTemplate extends React.Component<any, any> {
